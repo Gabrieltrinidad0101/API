2de7fe5a11c9fc05ac9facfc6d9a91f5185c420a
Gabriel <gabrielqwes123@gmail.com>
2023-08-26 20:48:04 -0400

    feat: document

--- frontend/src/screens/docs/infranstructure/components/generalDocs/GeneralDocs.module.tsx
+++ frontend/src/screens/docs/infranstructure/components/generalDocs/GeneralDocs.module.tsx
@@ -1,24 +1,44 @@
 import React from 'react'
 import ReactJson from 'react-json-view'
+import GeneralDocsCss from './GeneralDocs.module.css'
 export default function GeneralDocs (): JSX.Element {
   return (
-    <div>
+    <div className={GeneralDocsCss.container}>
       <div>
-        <div>URL</div>
+        <div className={GeneralDocsCss.header}>
+          <h2>Send Message</h2>
+          <div className={GeneralDocsCss.containerUrl}>
+            <span className={GeneralDocsCss.POST}>POST</span>
+            <span className={GeneralDocsCss.url}>
+              /{'{instanceId}'}/message/chat
+            </span>
+          </div>
+        </div>
         <div>
-          <ReactJson src={{ a: 1 }} theme='monokai' />
+          <ReactJson name={null} src={{
+            token: 'string',
+            body: 'string',
+            to: 'string'
+          }} theme='apathy' />
         </div>
       </div>
       <div>
-        <div>URL</div>
-        <div>
-          <ReactJson src={{ a: 1 }} theme='monokai' />
+        <div className={GeneralDocsCss.header}>
+          <h2>Send Document</h2>
+          <div className={GeneralDocsCss.containerUrl}>
+            <span className={GeneralDocsCss.POST}>POST</span>
+            <span className={GeneralDocsCss.url}>
+              /{'{instanceId}'}/message/document
+            </span>
+          </div>
         </div>
-      </div>
-      <div>
-        <div>URL</div>
         <div>
-          <ReactJson src={{ a: 1 }} theme='monokai' />
+          <ReactJson name={null} src={{
+            token: 'string',
+            to: 'string',
+            filename: 'string',
+            document: 'url'
+          }} theme='apathy' />
         </div>
       </div>
     </div>
