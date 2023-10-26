d14f93e91c21f20e3b4650193a8473a48affe367
Gabriel <gabrielqwes123@gmail.com>
2023-10-25 22:41:07 -0400

    feat: test

--- e2e/cypress/e2e/payInstance.cy.js
+++ e2e/cypress/e2e/payInstance.cy.js
@@ -7,17 +7,34 @@ describe("pay instance", () => {
         cy.get("#payInstanceLink")
             .invoke('attr', 'href')
             .then(href => {
-                cy.task("save",href)
+                cy.task("save", href)
             })
     })
 
     it("Payment", () => {
-        cy.task("load").then(link=>{
+        cy.task("load").then(link => {
             cy.visit(link)
-            cy.get("#email").type(conf.userSandBox)
+            cy.get("#email").type(conf.userSandBox, { delay: 100 })
             cy.get("#btnNext").click()
-            cy.get("#password").type(conf.userSandBox)
-            cy.get("#btnLogin").click()
+            cy.wait(10000)
+            cy.contains("$25.00")
         })
+    })
+})
+
+
+
+describe("Use instanced paymented", () => {
+    it("Send 101 messages", () => {
+        login();
+        cy.contains("Manager").click()
+        cy.get('input[name="to"]').type(phone)
+        cy.wait(500)
+        for (let i = 0; i < 101; i++) {
+            cy.get('textarea[name="body"]').type(i.toString())
+            cy.get("#buttonSendTestMessage").clear().click()
+            cy.wait(500)
+            cy.contains("SUCCESS").click()
+        }
     })
 })
